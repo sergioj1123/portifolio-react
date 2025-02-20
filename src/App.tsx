20b2c0c4ceeada35b434b1240268f80015f1ee63
Sergio Felix <sergiofelix9@gmail.com>
2025-02-20 02:21:42 -0300

    feat: adicionar integração com @vercel/analytics para monitoramento de desempenho

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <SpeedInsights />
+      <Analytics />
       <div className="fixed top-0 -z-10 h-full w-full ">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
