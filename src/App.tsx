69589f2d62ab6a2e40f030e6a4ffd2c68c6c8a45
Sergio Felix <sergiofelix9@gmail.com>
2025-02-20 02:18:04 -0300

    feat: adicionar integração com @vercel/speed-insights para análise de desempenho

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
@@ -10,6 +11,7 @@ import Tecnologies from "./components/Tecnologies";
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <SpeedInsights />
       <div className="fixed top-0 -z-10 h-full w-full ">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
